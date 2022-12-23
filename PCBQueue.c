51c3963ad3552dcff2a4c3f8ec9badb25d1b026f
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-12-23 05:12:40 +0200

    queue - not complete

---
+++ PCBQueue.c
@@ -0,0 +1,64 @@
+#include "PCBQueue.h"
+
+void enqueue(PCB* data)
+{
+    if(algo == PHPF)
+    {
+        
+    }
+
+    prQueue[++qSize] = data; //ignoring 0 index by post-increment
+    if(qSize == 1)
+        return;
+    if(algo == SRTN)
+    {
+        for(int i = qSize/2; i > 0; i--)
+            _heapifySRTN(i); //heapify from parent
+    }
+    else if(algo == SJF)
+    {
+        
+    }
+}
+
+PCB* dequeue()
+{
+    if(algo == PHPF)
+    {
+        
+    }
+
+    if(qSize == 0)
+        return NULL;
+    PCB* prcs = prQueue[1];
+    prQueue[1] = prQueue[qSize--];
+
+    if(qSize > 1)
+    {
+        if(algo == SRTN)
+        {
+            _heapifySRTN(1);
+        }
+        else if(algo == SJF)
+        {
+            
+        }
+    }
+    return prcs;
+
+}
+PCB* peak()
+{
+    if(qSize == 0)
+        return NULL;
+    return prQueue[1];
+}
+
+void _heapifySRTN(int index)
+{
+
+}
+void _heapifySJF(int index)
+{
+    
+}
---
+++ Queues/PCBQueue.h
@@ -0,0 +1,13 @@
+#include "../headers.h"
+
+
+Node *front = NULL, *rear = NULL;
+int qSize = 0;  //current number of elements in queue
+void enqueue(PCB* data);
+PCB* dequeue();
+PCB* peak();
+void _heapifySRTN(int index)
+void _heapifySJF(int index)
+
+
+
